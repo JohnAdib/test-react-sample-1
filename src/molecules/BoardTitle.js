2b3722f1b456e25247120c70add31d232de615aa
MrAdib <Mr.Javad.Adib@gmail.com>
2022-08-27 23:03:10 +0430

    change title fn to class

--- src/molecules/BoardTitle.js
+++ src/molecules/BoardTitle.js
@@ -1,13 +1,16 @@
+import React from "react";
 import H1 from "./../atoms/H1";
 
-function BoardTitle(props) {
-  var h1Class = "font-bold truncate " + props.color;
+class BoardTitle extends React.Component {
+  render() {
+    let h1Class = "font-bold truncate " + this.props.color;
 
-  return (
-    <div className='flex flex-row flwx-nowrap flex-none items-center gap-1 lg:gap-2 px-2 leading-10 rounded transition hover:bg-white/20 focus:bg-white/40'>
-      <H1 className={h1Class}>{props.children}</H1>
-    </div>
-  );
+    return (
+      <div className='flex flex-row flwx-nowrap flex-none items-center gap-1 lg:gap-2 px-2 leading-10 rounded transition hover:bg-white/20 focus:bg-white/40'>
+        <H1 className={h1Class}>{this.props.children}</H1>
+      </div>
+    );
+  }
 }
 
 export default BoardTitle;
