ca55b5cdc26bf6d354dabc8697edd3ee738757e7
MrAdib <Mr.Javad.Adib@gmail.com>
2022-08-27 16:58:10 +0430

    add molcules and organisms

---
+++ src/molecules/addList.js
@@ -0,0 +1,18 @@
+import "./../atoms/input";
+import "./../atoms/button";
+
+function addListForm(props) {
+  return (
+    <div>
+      <input
+        type='text'
+        value='Enter list title ...'
+        className='bg-blue-100'
+        onChnage=''
+      />
+      <button className='gb-blue-300'>Add list</button>
+    </div>
+  );
+}
+
+export default addListForm;
---
+++ src/organisms/lists.js
@@ -0,0 +1,11 @@
+import "./../molecules/addList";
+
+function lists(props) {
+  return (
+    <div className='bg-red-50'>
+      <addList />
+    </div>
+  );
+}
+
+export default lists;
