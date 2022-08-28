d9bf07cc9b14c048f48ac3ec79b8a077645cf65f
MrAdib <Mr.Javad.Adib@gmail.com>
2022-08-28 19:57:18 +0430

    show cards list

--- src/organisms/BoardLists.js
+++ src/organisms/BoardLists.js
@@ -3,27 +3,55 @@ import FormAddList from "./../molecules/FormAddList";
 function BoardLists(props) {
   return (
     <main className='grow p-2 px-6 w-full h-full flex gap-2 snap-x overflow-x-auto'>
-      <div className='snap-end shrink-0 relative basis-72'>
-        <img
-          className='relative shrink-0 w-180 h-40 rounded-lg shadow-xl bg-white'
-          src='https://images.unsplash.com/photo-1604999565976-8913ad2ddb7c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&h=160&q=80'
-        />
-      </div>
-      <div className='snap-end shrink-0 relative basis-72'>
-        <img
-          className='relative shrink-0 w-80 h-40 rounded-lg shadow-xl bg-white'
-          src='https://images.unsplash.com/photo-1540206351-d6465b3ac5c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=320&h=160&q=80'
-        />
-      </div>
-
-      <div className='snap-end shrink-0 relative basis-72'>
-        <FormAddList
-          onSubmitNewList={props.onSubmitNewList}
-          inputAddNewList={props.inputAddNewList}
-          onChangeInputAddNewList={props.onChangeInputAddNewList}
-        />
-      </div>
+      {listsLayout(props.data)}
+      {elAddNewList(props)}
     </main>
+  );
+}
+
+function listsLayout(lists) {
+  return lists.map((val) => (
+    <section className='snap-end shrink-0 relative basis-72' key={val.id}>
+      <div className='bg-gray-100/90 rounded leading-5 text-sm'>
+        {elCardTitlecard(val.title)}
+        {elCardDesing(val.cards)}
+        {elAddNewCard()}
+      </div>
+    </section>
+  ));
+}
+
+function elCardTitlecard(title) {
+  return (
+    <header className='p-2.5 font-semibold'>
+      <div className=''>{title}</div>
+    </header>
+  );
+}
+
+function elCardDesing(card) {
+  return (
+    <div class='p-2.5'>
+      {card.map((myCards) => (
+        <div>{myCards.title}</div>
+      ))}
+    </div>
+  );
+}
+
+function elAddNewCard(card) {
+  return <footer class='p-2.5'>"add a cards..."</footer>;
+}
+
+function elAddNewList(props) {
+  return (
+    <div className='snap-end shrink-0 relative basis-72'>
+      <FormAddList
+        onSubmitNewList={props.onSubmitNewList}
+        inputAddNewList={props.inputAddNewList}
+        onChangeInputAddNewList={props.onChangeInputAddNewList}
+      />
+    </div>
   );
 }
 
